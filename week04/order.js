972e5e558318b59202a001a38daba11a7b4b3c17
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-02-08 20:35:51 -0500

    Define discount variable in applyDiscount function
    Fix ReferenceError by defining the 'discount' variable.

--- week04/order.js
+++ week04/order.js
@@ -3,8 +3,12 @@ function calculateTotal(price, quantity) {
 }
 
 function applyDiscount(total) {
+  const discount = 10;
+
   if (total > 100) {
     return total - discount;
+    // ERROR: ReferenceError on line 7
+// Root cause: the variable "discount" is not defined anywhere in the program
   }
   return total;
 }
