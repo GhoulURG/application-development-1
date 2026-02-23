3b92d46a2ccd1e185fd909dd8933e0e782ac66c6
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-02-22 20:48:38 -0500

    Set up Express server with routes and middleware

--- week06/server.js
+++ week06/server.js
@@ -1,1 +1,31 @@
+const express = require("express");
+const app = express();
 
+const postsRoutes = require("./routes/posts");
+const usersRoutes = require("./routes/users");
+const logger = require("./middleware/logger");
+
+// JSON parsing
+app.use(express.json());
+
+// Logger middleware
+app.use(logger);
+
+// Routes
+app.use("/posts", postsRoutes);
+app.use("/users", usersRoutes);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    error: {
+      code: "NOT_FOUND",
+      message: "Route not found"
+    }
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
