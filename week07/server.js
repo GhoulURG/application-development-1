e23cb4878b59aa35dd3c30bbf5cb1ce53c215805
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-03-06 20:21:58 -0500

    Update server.js

--- week07/server.js
+++ week07/server.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const app = express();
 
-const timer = require("./middleware/timer");
-
-const postsRoutes = require("./routes/posts");
-const usersRoutes = require("./routes/users");
-
 app.use(express.json());
-app.use(timer);
 
-app.use("/posts", postsRoutes);
-app.use("/users", usersRoutes);
+const taskRoutes = require("./routes/tasks");
 
-app.use((req, res) => {
-  res.status(404).json({
-    error: {
-      code: "NOT_FOUND",
-      message: "Route not found"
-    }
-  });
-});
+app.use("/tasks", taskRoutes);
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
