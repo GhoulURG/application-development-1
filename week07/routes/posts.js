4112c145962f93595fd4ada6c2376974b8815a72
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-03-06 20:13:42 -0500

    Implement middleware for post routes
    Added middleware for API key authentication and post validation.

--- week07/routes/posts.js
+++ week07/routes/posts.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/postsController");
 
+const apiKeyAuth = require("../middleware/apiKeyAuth");
+const validatePost = require("../middleware/validatePost");
+
 router.get("/", controller.getAllPosts);
 router.get("/:id", controller.getPostById);
-router.post("/", controller.createPost);
-router.patch("/:id", controller.updatePost);
-router.delete("/:id", controller.deletePost);
+
+router.post("/", apiKeyAuth, validatePost, controller.createPost);
+
+router.patch("/:id", apiKeyAuth, controller.updatePost);
+
+router.delete("/:id", apiKeyAuth, controller.deletePost);
 
 module.exports = router;
