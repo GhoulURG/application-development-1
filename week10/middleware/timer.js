5677caf44f18e6345b68ad6e73c90b2b2e917c5b
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-03-30 12:09:18 -0400

    Create timer.js

---
+++ week10/middleware/timer.js
@@ -0,0 +1,10 @@
+module.exports = function timer(req, res, next) {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    console.log(`[${req.method} ${req.originalUrl}] completed in ${duration}ms`);
+  });
+
+  next();
+};
