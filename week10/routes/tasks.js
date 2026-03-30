0aca0864801292e3b7646ebafb86cee66d01ad49
Jacob Hill <134184174+GhoulURG@users.noreply.github.com>
2026-03-30 12:13:25 -0400

    Update tasks.js

--- week10/routes/tasks.js
+++ week10/routes/tasks.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/tasksController");
 
-const tasksController = require("../controllers/tasksController");
-const validateTask = require("../middleware/validateTask");
-
-// GET all tasks
-router.get("/", tasksController.getAllTasks);
-
-// GET single task
-router.get("/:id", tasksController.getTaskById);
-
-// POST create task
-router.post("/", validateTask, tasksController.createTask);
-
-// PATCH update task
-router.patch("/:id", validateTask, tasksController.updateTask);
-
-// DELETE task
-router.delete("/:id", tasksController.deleteTask);
+router.get("/", controller.getAllTasks);
+router.get("/:id", controller.getTaskById);
+router.post("/", controller.createTask);
+router.patch("/:id", controller.updateTask);
+router.delete("/:id", controller.deleteTask);
 
 module.exports = router;
